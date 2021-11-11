4454067b50c62202e2d81a12d6852fb0d228251d
mastercodingbear <mastercodingbear@gmail.com>
2021-11-11 22:08:08 +0200

    heroku url change

--- src/app/services/env.service.ts
+++ src/app/services/env.service.ts
@@ -5,9 +5,9 @@ import { Injectable } from '@angular/core';
 })
 export class EnvService {
   // eslint-disable-next-line @typescript-eslint/naming-convention
-  API_URL = 'http://192.168.114.14:5000/api';
+  // API_URL = 'http://192.168.114.14:5000/api';
   // API_URL = 'https://myalbum-app.herokuapp.com/api';
-  // API_URL = 'https://album-app-server.herokuapp.com/api';
+  API_URL = 'https://album-app-server.herokuapp.com/api';
   // eslint-disable-next-line @typescript-eslint/naming-convention
   PUBLICK_KEY = 'a6ce3a42c1d1be653179';
   // eslint-disable-next-line @typescript-eslint/naming-convention
